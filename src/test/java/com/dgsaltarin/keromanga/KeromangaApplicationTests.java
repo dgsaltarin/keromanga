fcfa2c42f47584e9f196cc130ff339977ade5942
dgsaltarin <dgsaltarin@gmail.com>
2021-03-29 06:36:16 -0500

    Initial commit

---
+++ src/test/java/com/dgsaltarin/keromanga/KeromangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsaltarin.keromanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeromangaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
