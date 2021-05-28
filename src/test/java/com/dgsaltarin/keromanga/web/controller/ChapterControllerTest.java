0542a0a9ea83194d6c50edffd5ee578e357f43b3
dgsaltarin <dgsaltarin@gmail.com>
2021-05-27 20:54:51 -0500

    added page controller tests

--- src/test/java/com/dgsaltarin/keromanga/web/controller/ChapterControllerTest.java
+++ src/test/java/com/dgsaltarin/keromanga/web/controller/ChapterControllerTest.java
@@ -3,7 +3,6 @@ package com.dgsaltarin.keromanga.web.controller;
 import com.dgsaltarin.keromanga.domain.Chapter;
 import com.dgsaltarin.keromanga.domain.data.ChapterRequest;
 import com.dgsaltarin.keromanga.domain.services.ChapterService;
-import org.assertj.core.internal.Bytes;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -20,21 +19,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
@@ -52,7 +50,6 @@ class ChapterControllerTest {
     private Chapter chapter2;
     private String request;
     private String badRequest;
-    private List<Bytes> pagesList;
     private int number;
     private int mangaId;
     private LocalDate date;
--- src/test/java/com/dgsaltarin/keromanga/web/controller/PageControllerTest.java
+++ src/test/java/com/dgsaltarin/keromanga/web/controller/PageControllerTest.java
@@ -5,31 +5,57 @@ import com.dgsaltarin.keromanga.domain.services.ChapterService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
 class PageControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+    @InjectMocks
     private PageController pageController;
+    @MockBean
     private ChapterService chapterService;
+
     private Page page;
+    private Page page2;
+    private String url;
     List<Page> mockList = new ArrayList<>();
 
     @BeforeAll
     public void setUp() {
         chapterService = Mockito.mock(ChapterService.class);
         pageController = new PageController(chapterService);
+        url = "page url";
+        page2 = new Page();
+        page2.setPageUrl(url);
         page = Mockito.mock(Page.class);
-        mockList.add(page);
+        mockList.add(page2);
+        mockMvc = MockMvcBuilders.standaloneSetup(pageController).build();
     }
 
     @Test
@@ -46,4 +72,20 @@ class PageControllerTest {
         assertEquals(HttpStatus.NOT_FOUND, pages.getStatusCode());
     }
 
+    @Test
+    public void shouldReturnPagesWhenFoundByChapterId() throws Exception {
+        when(chapterService.getChapterPages(any(Integer.class))).thenReturn(Optional.of(mockList));
+        mockMvc.perform(get("/page/chapter/{id}", 1))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].pageUrl", is(url)));
+    }
+
+    @Test
+    public void shouldReturnNotFoundWhenNoPagesFoundByChapterId() throws Exception {
+        when(chapterService.getChapterPages(any(Integer.class))).thenReturn(Optional.empty());
+        mockMvc.perform(get("/page/chapter/{id}", 1))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNotFound());
+    }
 }
