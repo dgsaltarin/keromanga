11f8f8654cc9594179efb2a0f4c13bb25eb6ddb1
dgsaltarin <dgsaltarin@gmail.com>
2021-07-18 14:37:01 -0500

    finished and fixed production configuration

--- src/main/java/com/dgsaltarin/keromanga/web/cloudinary/CloudinaryConfig.java
+++ src/main/java/com/dgsaltarin/keromanga/web/cloudinary/CloudinaryConfig.java
@@ -2,20 +2,10 @@ package com.dgsaltarin.keromanga.web.cloudinary;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CloudinaryConfig {
-
-  @Value("${CLOUD_NAME}")
-  private String cloudName;
-
-  @Value("${CLOUDINARY_SECRET}")
-  private String cloudSecret;
-
-  @Value("${CLOUDINARY_KEY}")
-  private String cloudKey;
 
   public Cloudinary cloudinary =
       new Cloudinary(
