866b0bb7cc44325721f677edbd871f98fdb61c78
dgsaltarin <dgsaltarin@gmail.com>
2021-07-18 11:19:18 -0500

    added  dotenv library to manage environment variables

--- src/main/java/com/dgsaltarin/keromanga/web/cloudinary/CloudinaryConfig.java
+++ src/main/java/com/dgsaltarin/keromanga/web/cloudinary/CloudinaryConfig.java
@@ -2,22 +2,27 @@ package com.dgsaltarin.keromanga.web.cloudinary;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CloudinaryConfig {
 
-    public Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "dgmanga",
-            "api_key", "489336662144792",
-            "api_secret", "NRP9AlZ24BtfhJZ84w7kErPk-dE",
-            "secure", true));
+  private Dotenv dotenv = Dotenv.load();
 
-    public Cloudinary getCloudinary() {
-        return cloudinary;
-    }
+  public Cloudinary cloudinary =
+      new Cloudinary(
+          ObjectUtils.asMap(
+              "cloud_name", dotenv.get("CLOUD_NAME"),
+              "api_key", dotenv.get("CLOUDINARY_KEY"),
+              "api_secret", dotenv.get("CLOUDINARY_SECRET"),
+              "secure", true));
 
-    public void setCloudinary(Cloudinary cloudinary) {
-        this.cloudinary = cloudinary;
-    }
+  public Cloudinary getCloudinary() {
+    return cloudinary;
+  }
+
+  public void setCloudinary(Cloudinary cloudinary) {
+    this.cloudinary = cloudinary;
+  }
 }
